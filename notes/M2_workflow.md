76092a1ef7d574e7bc916c392e6d58bd381f75bb
hurrialice <zhangq07@yahoo.com>
2019-09-06 10:57:30 -0400

    bug fix and add f1r2 filter

--- notes/M2_workflow.md
+++ notes/M2_workflow.md
@@ -4,19 +4,21 @@ This assumes you have got the PON with substantial normals
 
 ## params
 
-```
+```bash
 ref="/demo-mount/refs/Homo_sapiens_assembly19.fasta"
 germ="/demo-mount/refs/af-only-gnomad.raw.sites.b37.vcf"
 pon="/demo-mount/M2pon/makePON/AllinOne/merged_vcfs/fin_concat/AIO_merged_PON.vcf"
 interval=1 # a number denoting chromosome
-tumor= # a gs path
-normal=# a gs path
+tumor="" # a gs path
+normal="" # a gs path
 ```
 
 ### M2 for each pair
 
+#### scatter by chromosome
+
 ```bash
-gatk --java-options -Xmx2g \
+gatk --java-options -Xmx2g \ # so total 
 	-L 1 \ # need to scatter!
 	-R $ref \
 	-I $tumor \
@@ -24,6 +26,16 @@ gatk --java-options -Xmx2g \
 	--germline-resources $germ \
 	-pon $pon \
 	-O xxx.vcf # do not use gz!
+	--f1r2-tar-gz $f1r2
 ```
 
 use `.py` to generate a yaml for all tumor-normal pairs by `python3 M2scatter_genyaml.py` which will also produce the output directory.
+
+*[Note on directory structure]*
+
+* staging dir = `/demo-mount/canine_m2full/{pid}`
+* instruction yaml dir = `/demo-mount/canine_M2full_ostr`
+* output dir = `/demo-mount/M2full/{pid}`
+
+### Gather output and run filters
+
--- src/canine_related/M2scatter_genyaml.py
+++ src/canine_related/M2scatter_genyaml.py
@@ -22,7 +22,7 @@ with open(filepath) as f:
         pid_l.append(pid)
         cohort_l.append(cohort)
 
-
+chr_l = list(range(1, 23))+["X","Y"]
 basedir = "/demo-mount/M2full"
 stagedir = "/demo-mount/canine_m2full"
 ostrdir = "/demo-mount/canine_M2full_ostr"
@@ -30,26 +30,27 @@ ostrdir = "/demo-mount/canine_M2full_ostr"
 if os.path.exists(stagedir):
     raise Exception("staging dir exist!")
 
-for pid in pid_l:
+for pid, tumor, normal in zip(pid_l, tumor_l, normal_l):
     scatter_dir = "/".join([basedir,pid])
     if not os.path.exists(scatter_dir):
         os.makedirs(scatter_dir)
     
     yy = dict(
             name="M2full"+pid,
-            script=["gatk --java-options -Xmx2g Mutect2 -R $ref -I $tumor -I $normal -L $chr --germline-resource $germ -O $outfile -pon $pon"],
+            script=["gatk --java-options -Xmx2g Mutect2 -R $ref -I $tumor -I $normal -L $chr --germline-resource $germ -O $outfile -pon $pon --f1r2-tar-gz $f1r2"],
             resources={
                     "cpus_per_task":1,
-                    "mem_per_cpu":"2G"
+                    "mem_per_cpu":"4G"
                     },
             inputs={
                     "ref":"/demo-mount/refs/Homo_sapiens_assembly19.fasta",
                     "germ":"/demo-mount/refs/af-only-gnomad.raw.sites.b37.vcf",
-                    "tumor":tumor_l,
-                    "normal":normal_l,
-                    "chr": list(range(1,23)),
+                    "tumor":tumor,
+                    "normal":normal,
+                    "chr": chr_l,
                     "pon": "/demo-mount/M2pon/makePON/AllinOne/merged_vcfs/fin_concat/AIO_merged_PON.vcf",
-                    "outfile":["{}/{}_chr{}_somatic.vcf".format(scatter_dir,pid,chr) for chr in list(range(23))]
+                    "f1r2": ["{}/{}_chr{}-f1r2.tar.gz".format(scatter_dir,pid,chr) for chr in chr_l],
+                    "outfile":["{}/{}_chr{}_unfilter.vcf".format(scatter_dir,pid,chr) for chr in chr_l]
             },
     backend={
             "type":"Local",
@@ -65,6 +66,6 @@ for pid in pid_l:
                     }
             }
     )
-    with open("{}/canine_con_full.yaml".format(ostrdir),"w") as outfile:
+    with open("{}/{}-bychr.yaml".format(ostrdir,pid),"w") as outfile:
         yaml.dump(yy, outfile, default_flow_style=False)
     
